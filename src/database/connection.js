adecfe8687b26d96d4b4dac9d877e6966732fd2d
jenestiven <jhon.aricapa@correounivalle.edu.co>
2024-06-20 02:13:11 -0500

    chore: update database connection to use RDS endpoint

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,8 +5,11 @@ const pool = new Pool({
   user: "postgres",
   database: "resigcla-v1",
   password: "Alcol1rycoz",
-  //host: "localhost",
   host: "resigcla-v1.c0hz15humno9.us-east-1.rds.amazonaws.com",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  //host: "localhost",
   //port: 5432,
 });
 
