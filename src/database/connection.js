182963226199eecf3788843b10b3aaea82daaf29
jenestiven <jhon.aricapa@correounivalle.edu.co>
2024-06-19 22:48:06 -0500

    connect RDS

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,10 +3,11 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   user: "postgres",
-  database: "resigcla",
+  database: "resigcla-v1",
   password: "Alcol1rycoz",
-  host: "localhost",
-  port: 5432,
+  //host: "localhost",
+  host: "resigcla-v1.c0hz15humno9.us-east-1.rds.amazonaws.com",
+  //port: 5432,
 });
 
 export default pool;
